60af39448c9de58c3612bc8190ed75f78a48674c
JonasMohr <jones.m.987654321@gmail.com>
2021-03-28 21:44:08 +0200

    Initial commit

---
+++ src/main/java/com/github/nastygamer/statusdisplay/StatusDisplay.java
@@ -0,0 +1,10 @@
+package com.github.nastygamer.statusdisplay;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StatusDisplay implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
